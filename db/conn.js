7e01f2c427110ca44eeb75f58647ddc108f9a920
Abhidwivedi183 <abhishekdwivedi28062003@gmail.com>
2025-07-30 21:31:38 +0530

    some changes

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,7 @@
 const mong = require("mongoose");
+<<<<<<< HEAD
 mongoose.connect(process.env.MONGODB_URI)
+=======
+mong.connect("mongodb://localhost:27017/todoapp").then(()=>{console.log("connected")}).catch(()=>{"not connected"});
+>>>>>>> 5d3261d (some changes)
 
